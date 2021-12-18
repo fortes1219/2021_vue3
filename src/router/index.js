08e44d8c59a1f5f359c37a8ceb1b14e158e72da1
AstolfoProto <tokiya.kagami@gmail.com>
2021-12-18 14:59:34 +0800

    add files

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,11 @@ const routes = [
     path: '/',
     name: 'Home',
     component: () => import('../views/Home.vue')
+  },
+  {
+    path: '/provide',
+    name: 'Provide',
+    component: () => import('../views/Provide.vue')
   }
 ]
 
---
+++ src/views/Provide.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="page" data-inset="1rem">
+    <div class="row horizontal v_center">
+    <el-button type="primary" @click="getConfig">Click</el-button>
+    <el-button type="primary" @click="check">Check Data</el-button>
+    </div>
+    <div>{{ newObj.baseUrl }}</div>
+  </div>
+</template>
+
+<script>
+import {
+  ref,
+  reactive, 
+  computed,
+  onMounted,
+  provide,
+  readonly
+} from 'vue'
+export default {
+  name: 'Provide',
+  components: {
+  },
+  setup() {
+    let config = reactive({})
+    let newObj = reactive({})
+    const getConfig = async () => {
+      await fetch('http://localhost:8080/config.json').then(res => res.json()).then(res => {
+        config = {...res}
+      })
+    }
+    const check = () => {
+      newObj = {...config}
+      console.log(config,newObj)
+    }
+
+    return {
+      config,
+      newObj,
+      getConfig,
+      check
+    }
+  }
+}
+</script>
